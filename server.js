8e9fbc1a0a6ee962775b2df562f593dd31c004a1
Harsh <trivediharsh49@gmail.com>
2021-11-19 15:45:14 +0530

    stable commit for frontend

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/infix-to-postfix-frontend'));
+app.use(express.static('./dist/INFIXTOPOSTFIXFRONTEND'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/infix-to-postfix-frontend/'}),
+    res.sendFile('index.html', {root: 'dist/INFIXTOPOSTFIXFRONTEND/'}),
 );
 
 // Start the app by listening on the default Heroku port
